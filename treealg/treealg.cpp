c80fd80b7b0303ed5d96381db6382b99598a8e99
gilmi <65973449+gilmiwired@users.noreply.github.com>
2021-08-03 15:31:39 +0900

    Create treealg.cpp

---
+++ treealg/treealg.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct node{
+    int depth,name;
+    tree per;
+    vector<int> weed;
+}
+
+vector<note> tree;
+
+int main(){
+    
+
+}
+
