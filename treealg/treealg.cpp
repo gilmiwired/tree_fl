7c92b52f0171b56baf9aa7fb8bd3baebf4fcc00b
gilmi <65973449+gilmiwired@users.noreply.github.com>
2021-08-04 17:54:10 +0900

    a

--- treealg/treealg.cpp
+++ treealg/treealg.cpp
@@ -2,16 +2,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef struct node{
-    int depth,name;
-    tree per;
-    vector<int> weed;
-}
-
-vector<note> tree;
-
+typedef struct NODE{
+    int name;
+    string nm;
+    node* per;
+    vector<node*> weed;
+}node;
 int main(){
-    
+    vector<node> tree;
+    node a = node(a,1,"aa",&a,&a);
+    tree.push_back(a);
+    cout << tree[0].name << tree[0].nm;
+    return 0;
 
 }
 
+node setnode(node n , int name , string nm , node* per , vector<node*> weed){
+    n.name = name;
+    n.nm = nm;
+    n.per = per;
+    n.weed = weed;
+    return n;
+}
+//json に全部書き込み
